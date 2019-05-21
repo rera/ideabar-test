cc7ed49f48c4ea5854cd2df72b5f80dd97f12cf5
David Cabrera <david@rerainc.com>
2019-05-21 16:10:26 -0400

    wire up gulp and componentize sass

---
+++ Gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+gulp.task('styles', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('default', function () {
+  return gulp.watch('./sass/*.scss', gulp.series('styles'))
+})
