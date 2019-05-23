3c61b3db4fbd0b69d245dc9f9bad42131353341d
David Cabrera <david@rerainc.com>
2019-05-23 17:21:32 -0400

    update npm

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,16 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  livereload = require("gulp-livereload");
 
-gulp.task('styles', function () {
-  return gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'))
-})
+gulp.task("styles", function() {
+  return gulp
+    .src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css/"))
+    .pipe(livereload());
+});
 
-gulp.task('default', function () {
-  return gulp.watch('./sass/*.scss', gulp.series('styles'))
-})
+gulp.task("default", function() {
+  livereload.listen();
+  return gulp.watch("./sass/*.scss", gulp.series("styles"));
+});
